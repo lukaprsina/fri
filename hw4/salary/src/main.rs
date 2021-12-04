c7b774cd55d0422145164ccef42bba96ea9fe0b1
Luka Pršina <luk.prsina@gmail.com>
2021-12-04 19:58:40 +0100

    switch to linux

--- hw4/salary/src/main.rs
+++ hw4/salary/src/main.rs
@@ -14,6 +14,7 @@ fn read_n_lines(n: usize) -> Vec<String> {
 struct Employee {
     l: i32,
     r: i32,
+    s: i32,
 }
 
 #[derive(Debug)]
@@ -46,13 +47,27 @@ fn main() {
             test_cases[i as usize].e.push(Employee {
                 l: l_and_r[0],
                 r: l_and_r[1],
+                s: 0,
             });
         }
     }
 
-    println!("{:?}", &test_cases);
+    test_cases.iter_mut().for_each(|test_case| {
+        test_case.e.sort_by(|a, b| a.l.cmp(&b.l));
+        println!("{:?}", &test_case);
 
-    test_cases
-        .iter_mut()
-        .for_each(|test_case| test_case.e.sort_by(|a, b| a.l.cmp(&b.l)));
+        let mut median = (test_case.e.len() as f32 / 2.0f32).ceil() as i32 - 1;
+        let mut temp_sal = test_case.e[median as usize].l;
+
+        // test_case.e.iter().enumerate().for_each(|(pos, e)| {});
+
+        let found = false;
+        while !found {
+            if temp_sal < test_case.e[(median as usize) + 1].s {
+                test_case.e[median as usize].s += 1;
+            }
+        }
+    });
+
+    println!("{:?}", &test_cases);
 }
